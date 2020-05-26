69e191489318b3cd638d262170ba3bd94cbfdf71
sur5ive <karpo.korotko@gmail.com>
2020-05-26 09:18:51 +0300

    .

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Starting hvtwister"
-meteor --port 3010 --settings settings.json &>> /home/betfield/logs/hvtwister.log
+meteor --port 3003 --settings settings.json &>> /home/betfield/logs/hvtwister.log
 
